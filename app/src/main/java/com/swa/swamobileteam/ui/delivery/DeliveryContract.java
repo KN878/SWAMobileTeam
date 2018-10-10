616fbae30fe0973acf82d00e03e2ffa7d691bac2
Bulat Khabirov <khabiroff_b@me.com>
2018-10-10 23:25:50 +0300

    Implemented delivery groups model interface

--- app/src/main/java/com/swa/swamobileteam/ui/delivery/DeliveryContract.java
+++ app/src/main/java/com/swa/swamobileteam/ui/delivery/DeliveryContract.java
@@ -1,4 +1,19 @@
 package com.swa.swamobileteam.ui.delivery;
 
+import com.swa.swamobileteam.ui.base.BaseModel;
+import com.swa.swamobileteam.ui.base.BasePresenter;
+import com.swa.swamobileteam.ui.base.BaseView;
+
 public interface DeliveryContract {
+    interface View extends BaseView {
+
+    }
+
+    interface Presenter extends BasePresenter<View> {
+
+    }
+
+    interface Model extends BaseModel {
+
+    }
 }
