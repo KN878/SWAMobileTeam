8a45aaf7e3fee80b979824aa62c03e6f425fe008
Bulat Khabirov <khabiroff_b@me.com>
2018-10-11 02:09:08 +0300

    Models and interfaces for delivery details module

---
+++ app/src/main/java/com/swa/swamobileteam/data/deliveries/ClientInfo.java
@@ -0,0 +1,32 @@
+package com.swa.swamobileteam.data.deliveries;
+
+/**
+ * Information about a client a parcel is intended to be delivered to.
+ */
+public class ClientInfo {
+    private String firstName;
+    private String lastName;
+
+    /**
+     * Phone number string with country code.
+     */
+    private String phoneNumber;
+
+    public ClientInfo(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
