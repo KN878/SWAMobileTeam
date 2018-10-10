e98a20f0838757ba090cfe4aaeb8945b1f9f4428
Bulat Khabirov <khabiroff_b@me.com>
2018-10-11 01:20:36 +0300

    Implemented models required for the list

--- app/src/main/java/com/swa/swamobileteam/ui/deliveryGroups/DeliveriesListItem.java
+++ app/src/main/java/com/swa/swamobileteam/ui/deliveryGroups/DeliveriesListItem.java
@@ -1,4 +1,38 @@
 package com.swa.swamobileteam.ui.deliveryGroups;
 
+import com.swa.swamobileteam.data.deliveries.Address;
+import com.swa.swamobileteam.data.deliveries.DeliveryPeriod;
+
 public class DeliveriesListItem {
+    private String id;
+    private Address address;
+    private DeliveryPeriod deliveryPeriod;
+    private Boolean isInProgress;
+
+    public DeliveriesListItem(String id, Address address, DeliveryPeriod deliveryPeriod, Boolean isInProgress) {
+        this.id = id;
+        this.address = address;
+        this.deliveryPeriod = deliveryPeriod;
+        this.isInProgress = isInProgress;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public DeliveryPeriod getDeliveryPeriod() {
+        return deliveryPeriod;
+    }
+
+    public Boolean getInProgress() {
+        return isInProgress;
+    }
+
+    public void setInProgress(Boolean inProgress) {
+        isInProgress = inProgress;
+    }
 }
