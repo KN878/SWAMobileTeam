7ed8a658bd4d973d87aaccbe04cd5da372fa13c8
Unknown <rozaliya.amirova@gmail.com>
2018-10-25 12:47:15 +0300

    Models added
    DeliveryScheduleModel, DeliveryOrderModel, ControlOperator and DeliveryScheduleReponse, SeliveryOrderResponse, ControlOperatorResponse respectively

---
+++ app/src/main/java/com/swa/swamobileteam/data/deliveries/DeliveryOrderResponse.java
@@ -0,0 +1,4 @@
+package com.swa.swamobileteam.data.deliveries;
+
+public class DeliveryOrderResponse {
+}
---
+++ app/src/main/java/com/swa/swamobileteam/data/deliveries/HumanContacts.java
@@ -0,0 +1,7 @@
+package com.swa.swamobileteam.data.deliveries;
+
+public class HumanContactsImpl {
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+}
